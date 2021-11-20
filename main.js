bc1c717d4ebf013c24f17e16a1affdc8e173f704
msnpy <53246704+msnpy@users.noreply.github.com>
2021-11-20 17:30:29 -0300

    hello

---
+++ main.js
@@ -0,0 +1,26 @@
+//video
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+
+
+//audio
+var x = document.getElementById("myAudio"); 
+
+function playAudio() { 
+  x.play(); 
+} 
+
+function pauseAudio() { 
+  x.pause(); 
+} 
